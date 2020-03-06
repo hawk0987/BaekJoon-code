4841cb08b49423479708f7bd972bf4eaf8a96c9e
오승환 <52457475+hawk0987@users.noreply.github.com>
2020-03-06 15:34:58 +0900

    Create num_10808.java

---
+++ num_10808.java
@@ -0,0 +1,193 @@
+package String;
+import java.util.*;
+public class num_10808 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String S = scan.nextLine();
+		int a=0,b=0,c=0,d=0,e=0,f=0,g=0,h=0,i=0,j=0,k=0,l=0,m=0,n=0,o=0,p=0,q=0,r=0,s=0,t=0,u=0,v=0,w=0,x=0,y=0,z=0;
+		
+		for(int i1=0;i1<S.length();i1++) {
+			if(S.charAt(i1)=='a') {
+				a++;
+			}
+		}
+		System.out.print(a + " ");
+		
+		for(int i1=0;i1<S.length();i1++) {
+			if(S.charAt(i1)=='b') {
+				b++;
+			}
+		}
+		System.out.print(b + " ");
+		
+		for(int i1=0;i1<S.length();i1++) {
+			if(S.charAt(i1)=='c') {
+				c++;
+			}
+		}
+		System.out.print(c + " ");
+		
+		for(int i1=0;i1<S.length();i1++) {
+			if(S.charAt(i1)=='d') {
+				d++;
+			}
+		}
+		System.out.print(d + " ");
+		
+		for(int i1=0;i1<S.length();i1++) {
+			if(S.charAt(i1)=='e') {
+				e++;
+			}
+		}
+		System.out.print(e + " ");
+		
+		for(int i1=0;i1<S.length();i1++) {
+			if(S.charAt(i1)=='f') {
+				f++;
+			}
+		}
+		System.out.print(f + " ");
+		
+		for(int i1=0;i1<S.length();i1++) {
+			if(S.charAt(i1)=='g') {
+				g++;
+			}
+		}
+		System.out.print(g + " ");
+		
+		for(int i1=0;i1<S.length();i1++) {
+			if(S.charAt(i1)=='h') {
+				h++;
+			}
+		}
+		System.out.print(h + " ");
+		
+		for(int i1=0;i1<S.length();i1++) {
+			if(S.charAt(i1)=='i') {
+				i++;
+			}
+		}
+		System.out.print(i + " ");
+		
+		for(int i1=0;i1<S.length();i1++) {
+			if(S.charAt(i1)=='j') {
+				j++;
+			}
+		}
+		System.out.print(j + " ");
+		
+		for(int i1=0;i1<S.length();i1++) {
+			if(S.charAt(i1)=='k') {
+				k++;
+			}
+		}
+		System.out.print(k + " ");
+		
+		for(int i1=0;i1<S.length();i1++) {
+			if(S.charAt(i1)=='l') {
+				l++;
+			}
+		}
+		System.out.print(l + " ");
+		
+		for(int i1=0;i1<S.length();i1++) {
+			if(S.charAt(i1)=='m') {
+				m++;
+			}
+		}
+		System.out.print(m + " ");
+		
+		for(int i1=0;i1<S.length();i1++) {
+			if(S.charAt(i1)=='n') {
+				n++;
+			}
+		}
+		System.out.print(n + " ");
+		
+		for(int i1=0;i1<S.length();i1++) {
+			if(S.charAt(i1)=='o') {
+				o++;
+			}
+		}
+		System.out.print(o + " ");
+		
+		for(int i1=0;i1<S.length();i1++) {
+			if(S.charAt(i1)=='p') {
+				p++;
+			}
+		}
+		System.out.print(p + " ");
+		
+		for(int i1=0;i1<S.length();i1++) {
+			if(S.charAt(i1)=='q') {
+				q++;
+			}
+		}
+		System.out.print(q + " ");
+		
+		for(int i1=0;i1<S.length();i1++) {
+			if(S.charAt(i1)=='r') {
+				r++;
+			}
+		}
+		System.out.print(r + " ");
+		
+		for(int i1=0;i1<S.length();i1++) {
+			if(S.charAt(i1)=='s') {
+				s++;
+			}
+		}
+		System.out.print(s + " ");
+		
+		for(int i1=0;i1<S.length();i1++) {
+			if(S.charAt(i1)=='t') {
+				t++;
+			}
+		}
+		System.out.print(t + " ");
+		
+		for(int i1=0;i1<S.length();i1++) {
+			if(S.charAt(i1)=='u') {
+				u++;
+			}
+		}
+		System.out.print(u + " ");
+		
+		for(int i1=0;i1<S.length();i1++) {
+			if(S.charAt(i1)=='v') {
+				v++;
+			}
+		}
+		System.out.print(v + " ");
+		
+		for(int i1=0;i1<S.length();i1++) {
+			if(S.charAt(i1)=='w') {
+				w++;
+			}
+		}
+		System.out.print(w + " ");
+		
+		for(int i1=0;i1<S.length();i1++) {
+			if(S.charAt(i1)=='x') {
+				x++;
+			}
+		}
+		System.out.print(x + " ");
+		
+		for(int i1=0;i1<S.length();i1++) {
+			if(S.charAt(i1)=='y') {
+				y++;
+			}
+		}
+		System.out.print(y + " ");
+		
+		for(int i1=0;i1<S.length();i1++) {
+			if(S.charAt(i1)=='z') {
+				z++;
+			}
+		}
+		System.out.print(z);
+		
+	}
+
+}
