b56e8d79d018615e204b1e656f7cd4c223bd9def
오승환 <52457475+hawk0987@users.noreply.github.com>
2020-03-06 15:32:13 +0900

    Create num_16486.java

---
+++ num_16486.java
@@ -0,0 +1,11 @@
+package Math1;
+import java.util.*;
+public class num_16486 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int d1 = scan.nextInt();
+		int d2 = scan.nextInt();
+		
+		System.out.println(d1*2 + 2*d2*3.141592);
+	}
+}
