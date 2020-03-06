e152089c3a42576b9b60bb20810e5372776bbbbb
오승환 <52457475+hawk0987@users.noreply.github.com>
2020-03-06 15:29:09 +0900

    num_9316
    백준 9316번 정답 소스코드

---
+++ num_9316.java
@@ -0,0 +1,11 @@
+package basic;
+import java.util.*;
+public class num_9316 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int N = scan.nextInt();
+		for(int i=0;i<N;i++) {
+			System.out.println("Hello World, Judge "+(i+1)+"!");
+		}
+	}
+}
