3e05eb818d1c55a2adbf7f01b7759ab38b8e4c85
오승환 <52457475+hawk0987@users.noreply.github.com>
2020-03-06 15:32:14 +0900

    Create num_16484.java

---
+++ num_16484.java
@@ -0,0 +1,12 @@
+package Math1;
+import java.util.*;
+public class num_16484 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		int d = scan.nextInt();
+		
+		double r = n/2.0;
+		System.out.println(r-d);		
+	}
+}
