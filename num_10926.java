e53e30eb5d8928ddbcb89b43f36917974be1fde8
오승환 <52457475+hawk0987@users.noreply.github.com>
2020-03-06 15:33:02 +0900

    Create num_10926.java

---
+++ num_10926.java
@@ -0,0 +1,9 @@
+package print;
+import java.util.*;
+public class num_10926 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String S = scan.next();
+		System.out.println(S+"??!");
+	}
+}
