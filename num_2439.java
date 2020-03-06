373457965826ae564ec801d4f80ace8c0d0cdfa2
오승환 <52457475+hawk0987@users.noreply.github.com>
2020-03-06 15:31:13 +0900

    Create num_2439.java

---
+++ num_2439.java
@@ -0,0 +1,20 @@
+package For;
+import java.util.Scanner;
+public class num_2439 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int num;
+		num = scan.nextInt();
+		for(int i=0;i<num;i++) {
+			for (int j=0;j<num-i-1;j++)
+				System.out.print(" ");
+			for(int j=0;j<=i;j++)
+				System.out.print("*");
+				System.out.println("");
+		}
+		
+	}
+
+}
