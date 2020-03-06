82963c6bd2b98fa98033e6ef6ed715a9090fb496
오승환 <52457475+hawk0987@users.noreply.github.com>
2020-03-06 15:34:39 +0900

    Create num_11654.java

---
+++ num_11654.java
@@ -0,0 +1,12 @@
+package String;
+import java.util.*;
+public class num_11654 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int ch;
+		ch = scan.nextLine().charAt(0);
+		
+		System.out.println(ch);
+		
+	}
+}
