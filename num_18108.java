1e2b9246c789faeb46844d20387bc00841d18eb5
오승환 <52457475+hawk0987@users.noreply.github.com>
2020-03-06 15:30:32 +0900

    Create num_18108.java

---
+++ num_18108.java
@@ -0,0 +1,10 @@
+package beginner;
+import java.util.*;
+public class num_18108 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int y = scan.nextInt();
+		System.out.println(y-543);
+		
+	}
+}
