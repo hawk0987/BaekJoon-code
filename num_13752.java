a112bb073b457029ab9c87dbc3d1346f5ffe978b
오승환 <52457475+hawk0987@users.noreply.github.com>
2020-03-06 15:31:26 +0900

    Create num_13752.java

---
+++ num_13752.java
@@ -0,0 +1,18 @@
+package ICPC;
+import java.util.*;
+public class num_13752 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		
+		for(int i=0;i<n;i++) {
+			
+			int k = scan.nextInt();
+			
+			for(int j=0;j<k;j++) {
+				System.out.print("=");
+			}
+			System.out.println();
+		}		
+	}
+}
