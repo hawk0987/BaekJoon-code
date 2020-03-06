6a4413311895857099596e4f053335b125e4e805
오승환 <52457475+hawk0987@users.noreply.github.com>
2020-03-06 15:33:08 +0900

    Create num_5337.java

---
+++ num_5337.java
@@ -0,0 +1,9 @@
+package print;
+public class num_5337 {
+	public static void main(String[] args) {
+		System.out.println(".  .   .\n" + 
+				"|  | _ | _. _ ._ _  _\n" + 
+				"|/\\|(/.|(_.(_)[ | )(/.");
+
+	}
+}
