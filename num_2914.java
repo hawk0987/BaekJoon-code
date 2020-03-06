e1dbbf8cbc440b124354526f6bc10d3903df55ed
오승환 <52457475+hawk0987@users.noreply.github.com>
2020-03-06 15:32:06 +0900

    Create num_2914.java

---
+++ num_2914.java
@@ -0,0 +1,11 @@
+package Math1;
+import java.util.*;
+public class num_2914 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int A = scan.nextInt();
+		int I = scan.nextInt();
+		
+		System.out.println(A*(I-1)+1);
+	}
+}
