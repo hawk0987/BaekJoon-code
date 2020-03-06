5b9c88a375b08fb117f6cd83c624a1978f2bfb5b
오승환 <52457475+hawk0987@users.noreply.github.com>
2020-03-06 15:27:25 +0900

    num_16430
    백준 16430번 정답 소스코드

---
+++ num_16430.java
@@ -0,0 +1,13 @@
+package basic;
+import java.util.*;
+public class num_16430 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int son = scan.nextInt();
+		int mom = scan.nextInt();
+		
+		son = mom-son;
+		System.out.println(son + " " + mom);
+
+	}
+}
