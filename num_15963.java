893b8148f54917d441ed5231a032a1088abd27a9
오승환 <52457475+hawk0987@users.noreply.github.com>
2020-03-06 15:27:00 +0900

    num_15963
    백준 15963번 정답 소스코드

---
+++ num_15963.java
@@ -0,0 +1,16 @@
+package basic;
+import java.util.*;
+public class num_15963 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		long N = scan.nextLong();	
+		long M = scan.nextLong();
+		
+		if(N==M) {
+			System.out.println("1");
+		}
+		else {
+			System.out.println("0");	
+		}
+	}
+}
