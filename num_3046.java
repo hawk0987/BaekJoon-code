88763401096c4cdaa9be70195e0707b7a302fa8e
오승환 <52457475+hawk0987@users.noreply.github.com>
2020-03-06 15:32:08 +0900

    Create num_3046.java

---
+++ num_3046.java
@@ -0,0 +1,13 @@
+package Math1;
+import java.util.*;
+public class num_3046 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int R1 = scan.nextInt();
+		int S = scan.nextInt();
+		
+		int R2 = S*2 -R1;
+		System.out.println(R2);
+
+	}
+}
