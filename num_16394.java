11d1fee175e3fee3a473e573096e203fc6091efc
오승환 <52457475+hawk0987@users.noreply.github.com>
2020-03-06 15:27:17 +0900

    num_16394
    백준 16394번 정답 소스코드

---
+++ num_16394.java
@@ -0,0 +1,11 @@
+package basic;
+import java.util.*;
+public class num_16394 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int N = scan.nextInt();
+		System.out.println(N-1946);
+
+	}
+}
