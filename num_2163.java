ee3b784c65cd23fc9e4a11c32ff2f85d9711e474
오승환 <52457475+hawk0987@users.noreply.github.com>
2020-03-06 15:30:38 +0900

    Create num_2163.java

---
+++ num_2163.java
@@ -0,0 +1,11 @@
+package beginner;
+import java.util.*;
+public class num_2163 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int N = scan.nextInt();
+		int M = scan.nextInt();
+		int num = (N*M)-1;
+		System.out.println(num);
+	}
+}
