6b1b31c731a8d224cbbfcc15ec6cd87f9e7f8cad
오승환 <52457475+hawk0987@users.noreply.github.com>
2020-03-06 15:33:24 +0900

    Create num_11718.java

---
+++ num_11718.java
@@ -0,0 +1,12 @@
+package print;
+import java.util.*;
+public class num_11718 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String S;
+		while(scan.hasNextLine()) {
+			S = scan.nextLine();	
+			System.out.println(S);
+		}
+	}
+}
